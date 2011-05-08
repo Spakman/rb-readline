050b768d11817ca4f80f664479aeee8a0d8ff41c
Mark Somerville <mark@scottishclimbs.com>
2011-05-08 14:16:10 -0700

    Ensure rl_filename_completion_function uses dequoted path
    This fixes a bug where, although @rl_filname_dequoting_function was
    called, the dequoted path never made it as far as the opendir() system
    call, essentially making it useless. This problem was also acknowledged
    in GNU Readline[1], and was addressed in version 6.2.
    
    1 - http://lists.gnu.org/archive/html/bug-readline/2010-03/msg00011.html
    
    Tested-by: Mark Somerville <mark@scottishclimbs.com> [MRI 1.8.7, MRI 1.9.2]
    Signed-off-by: Mark Somerville <mark@scottishclimbs.com>

--- test/test_filename_completion_proc.rb
+++ test/test_filename_completion_proc.rb
@@ -1,55 +1,16 @@
 require 'test/unit'
-require 'fileutils'
 require 'readline'
+require "#{File.expand_path(File.dirname(__FILE__))}/filesystem_completion_helper"
 
 class TC_FILENAME_COMPLETION_PROC < Test::Unit::TestCase
+  include FilesystemCompletionHelper
 
-  SEP = File::SEPARATOR
-  COMP_TEST_DIR = "comp_test#{SEP}"
-  SUB_DIR = "#{COMP_TEST_DIR}a_sub_dir#{SEP}"
-  SUB_SUB_DIR = "#{SUB_DIR}another_sub_dir#{SEP}"
-  DIR_WITH_SPACES = "#{COMP_TEST_DIR}dir with spaces#{SEP}"
-  SUB_DIR_WITH_SPACES = "#{DIR_WITH_SPACES}sub_dir with spaces#{SEP}"
-
-  # This creates:
-  #
-  #   comp_test/
-  #     abc
-  #     aaa
-  #     a_sub_dir/
-  #       abc
-  #       aaa
-  #       another_sub_dir/
-  #         aaa
-  #     dir with spaces/
-  #       filename with spaces
-  #       sub dir with spaces/
-  #         another filename with spaces
   def setup
-    FileUtils.mkdir_p("#{SUB_SUB_DIR}")
-    FileUtils.mkdir_p("#{SUB_DIR_WITH_SPACES}")
-    @comp_test_dir = Dir.new COMP_TEST_DIR
-    @sub_dir = Dir.new SUB_DIR
-    @sub_sub_dir = Dir.new SUB_SUB_DIR
-    @dir_with_spaces = Dir.new DIR_WITH_SPACES
-    @sub_dir_with_spaces = Dir.new SUB_DIR_WITH_SPACES
-
-    FileUtils.touch("#{@comp_test_dir.path}abc")
-    FileUtils.touch("#{@comp_test_dir.path}aaa")
-    FileUtils.touch("#{@sub_dir.path}abc")
-    FileUtils.touch("#{@sub_dir.path}aaa")
-    FileUtils.touch("#{@sub_sub_dir.path}aaa")
-    FileUtils.touch("#{@dir_with_spaces.path}filename with spaces")
-    FileUtils.touch("#{@sub_dir_with_spaces.path}another filename with spaces")
-
-    # The previous Dir.new calls seem to cache the dir entries on Windows.
-    @comp_test_dir = Dir.new COMP_TEST_DIR
-    @sub_dir = Dir.new SUB_DIR
-    @sub_sub_dir = Dir.new SUB_SUB_DIR
+    setup_filesystem_for_completion
   end
 
   def teardown
-    FileUtils.rm_r(COMP_TEST_DIR)
+    teardown_filesystem_after_completion
   end
 
   def test_listing_files_in_cwd
